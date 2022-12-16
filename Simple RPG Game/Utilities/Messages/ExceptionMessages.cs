3ba9e976c762e5b19ab6777cfc30d9a8cd176f39
venci0003 <venci0003@gmail.com>
2022-12-16 18:07:23 +0200

    Added Utilities folder with "Exception" messages and "Output" messages TODO add specific exceptions and outputs

---
+++ Simple RPG Game/Utilities/Messages/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace BookingApp.Utilities.Messages
+{
+    public static class ExceptionMessages
+    {
+        public const string PriceCantBeNegative = "Price cannot be negative!";
+
+        public const string DurationZeroOrLess = "Duration cannot be negative or zero!";
+
+        public const string RoomTypeIncorrect = "Incorrect room type!";
+
+        public const string NotEnoughCapacity = "Not enough capacity.";
+
+        public const string NameCannotBeNullOrWhiteSpace = "Invalid name input!";
+    }
+}
---
+++ Simple RPG Game/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace BookingApp.Utilities.Messages
+{
+    public static class OutputMessages
+    {
+        public const string NameAlreadyExists = "Name {0} is already registered in our platform!";
+    }
+}
